d986855e766b6ddc4cb6178e324e4be99243a2ab
dartchuk <manson1989>
2019-02-28 23:58:04 +0200

    complete task 1

--- php/task1.php
+++ php/task1.php
@@ -1,8 +1,13 @@
 <?php
 declare(strict_types=1);
 
-function spam(int $number = 0) :string {
-    return str_repeat ('hue', $number);
+/**
+ * @param int $number
+ * @return string
+ */
+function spam(int $number = 0): string
+{
+    return str_repeat('hue', $number);
 }
 
 
--- php/task2.php
+++ php/task2.php
@@ -1,2 +1,37 @@
 <?php
+declare(strict_types=1);
 
+$exampleString = 'camelCasing';
+
+/**
+ * @param string $input
+ * @return string
+ */
+function solution(string $input): string
+{
+    $arrayFromInput = splitString($input);
+    $implodeInput = implode($arrayFromInput);
+
+    return mb_strtolower($implodeInput);
+}
+
+/**
+ * @param string $string
+ * @return array
+ */
+function splitString(string $string): array
+{
+    $res = [];
+    $length = strlen($string);
+
+    for ($i=0; $i<$length; $i++) {
+        if (preg_match('/[A-Z]/', $string[$i])) {
+            $res[] = ' ';
+        }
+        $res[] = $string[$i];
+    }
+
+    return $res;
+}
+
+echo solution($exampleString);
