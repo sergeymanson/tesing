827c44dcd42af3696665808f3ba2e73e9d2a644d
dartchuk <manson1989>
2019-02-28 22:59:18 +0200

    complete task 1

--- php/task1.php
+++ php/task1.php
@@ -1,2 +1,13 @@
 <?php
+declare(strict_types=1);
 
+function spam(int $number = 0) :string {
+    return str_repeat ('hue', $number);
+}
+
+
+echo spam(1);
+echo '</br>';
+echo spam(6);
+echo '</br>';
+echo spam(14);
