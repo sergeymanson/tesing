e57a27b3e3d070d649a1788c04e8793f0700621a
dartchuk <manson1989>
2019-03-03 12:23:39 +0200

    complete task 1

--- js/task2.js
+++ js/task2.js
@@ -1,2 +1,34 @@
+const constant = a => (f = x => x) => ({
+  [Symbol.toPrimitive](hint) {
+    return eval(this.valueOf());
+  },
+
+  valueOf() {
+    return f(a);
+  }
+});
+
+const operator  = sign => b => a => {
+  return `${a.valueOf()} ${sign} ${b.valueOf()}`
+};
+
+const one   = constant(1);
+const two   = constant(2);
+const three = constant(3);
+const five  = constant(5);
+const six   = constant(6);
+const eight = constant(8);
 
 
+const plus      = operator("+");
+const minus     = operator("-");
+const times     = operator("*");
+const dividedBy = operator("/");
+
+
+alert([
+  three(times(five())),                      // 3 * 5
+  // eight(minus(two())),                       // 8 - 2
+  // one(plus(six(dividedBy(three())))),        // 1 + 6 / 3
+  // five(minus(two(minus(two(minus(one())))))) // 5 - 2 - 2 - 1
+]);
