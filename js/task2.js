95ca6a7bea96c1d478c7f59fbcef943be96a36ad
dartchuk <manson1989>
2019-03-05 00:23:46 +0200

    complete task 3

--- js/task2.js
+++ js/task2.js
@@ -1,63 +1,16 @@
-// const constant = a => {
-//   value: null,
-//   [Symbol.toPrimitive](hint) {
-//     debugger
-//     return eval(this.valueOf());
+
+
+// function zero (x) {
+//   let value = 0;
+//
+//   if (value === null) {
+//     value = x;
 //   }
 //
-//   // valueOf() {
-//   //   debugger
-//   //   return f(a);
-//   // }
-// };
-//
-// const figure = {
-//   id: 1,
-//   name: 'Прямоугольник',
-//   [Symbol.toPrimitive](hint) {
-//     if (hint === 'string') {
-//       return this.name;
-//     } else if (hint === 'number') {
-//       return this.id;
-//     } else {  // default
-//       return this.name;
-//     }
-//   }
-// }
-
-function zero (x) {
-  let value = 0;
-
-  if (value === null) {
-    value = x;
-  }
-
-  return String(value)
-  debugger
-};
-
-//
-// const operator  = sign => b => a => {
+//   return String(value)
 //   debugger
-//   return `${a.valueOf()} ${sign} ${b.valueOf()}`
 // };
-//
-// const zero   = constant(0);
-// const one   = constant(1);
-// const two   = constant(2);
-// const three = constant(3);
-// const four = constant(4);
-// const five  = constant(5);
-// const six   = constant(6);
-// const seven   = constant(7);
-// const eight = constant(8);
 
-
-// const plus      = operator("+");
-// const minus     = operator("-");
-// const times     = operator("*");
-// const dividedBy = operator("/");
-//
 
 
 // checkCalculations = () => {
