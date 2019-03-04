ec02d6334dfef9c8cfcc2ec4f6c6b5e6b8a3b497
dartchuk <manson1989>
2019-03-04 23:23:38 +0200

    complete task 3

--- php/task3.php
+++ php/task3.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 $arr = [
     '123 456', // 0
@@ -6,45 +7,54 @@ $arr = [
     '123 594' // 1
 ];
 
-function main($array)
+/**
+ * @param array $array
+ * @return array
+ */
+function carry(array $array): array
 {
     $res = [];
 
     foreach ($array as $item) {
-        $res[] = carry(explode(' ', $item));
+        $res[] = countCarry(explode(' ', $item));
     }
 
     return $res;
 }
 
-
-function carry($array)
+/**
+ * @param array $array
+ * @return int
+ */
+function countCarry(array $array): int
 {
     $res = 0;
-    $loop = true;
     $a = intval($array[0]);
     $b = intval($array[1]);
 
-    while ($loop && ($a > 0 || $b > 0)) {
-
-        if (($a % 10 + $b % 10) / 10 >= 1) {
+    while ($a > 0 || $b > 0) {
+        if (hasCarry($a, $b)) {
             $res++;
         }
-
         $a = intval($a / 10);
         $b = intval($b / 10);
 
-        if ($a == 0 && $b == 0) {
-            $loop = false;
-        }
     }
 
     return $res;
 }
 
+/**
+ * @param $a
+ * @param $b
+ * @return bool
+ */
+function hasCarry(int $a, int $b): bool
+{
+    return ($a % 10 + $b % 10) / 10 >= 1;
+}
 
-
-foreach (main($arr) as $item) {
+foreach (carry($arr) as $item) {
     echo $item;
     echo '<br>';
 }
